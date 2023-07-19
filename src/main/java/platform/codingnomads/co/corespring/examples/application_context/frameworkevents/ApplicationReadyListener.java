6fb5ea65d292d3736481c3b19527a6b383b9e294
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-07-19 17:44:35 +0200

    ApplicationContext EventListeners exercises done

---
+++ src/main/java/platform/codingnomads/co/corespring/examples/application_context/frameworkevents/ApplicationReadyListener.java
@@ -0,0 +1,14 @@
+package platform.codingnomads.co.corespring.examples.application_context.frameworkevents;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
+        System.out.println("Application Ready!");
+    }
+}
